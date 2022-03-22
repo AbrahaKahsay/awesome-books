cd4d7493a99f4b352b10b130ad56b934281d5b42
Abraha Kahsay <abrahak81@gmail.com>
2022-03-22 03:07:58 +0300

    Adding forms in html and objects in js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const insertBookSection = document.querySelector('.insert-book');
+
+function CreateBooks(title, author){
+    this.title = title;
+    this.author = author;
+}
+
+const firstBook = new CreateBooks("The Alchemist", "Paulo Cohleo");
+
+
+
