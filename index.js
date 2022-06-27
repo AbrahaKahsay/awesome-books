ad84e834b77b582dd3bbd627067e513002861bb7
wayungi <fwayungi@gmail.com>
2022-06-27 23:17:37 +0300

    Add temporarly array of books

---
+++ index.js
@@ -0,0 +1,20 @@
+const books = [
+    { 
+        title: 'Easy Code',
+        author: 'Abraha',
+        id:1
+    },
+
+    {
+        title: 'Hard Code',
+        author: 'Diego',
+        id:2
+    },
+
+    {
+        title: 'Javacript for all',
+        author: 'Francis',
+        id:3
+    }
+
+]
