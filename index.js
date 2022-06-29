3327a3512469b377a01126737f1ab05de2867875
wayungi <fwayungi@gmail.com>
2022-06-29 23:29:33 +0300

    Fix remove book error

--- index.js
+++ index.js
@@ -76,21 +76,20 @@ class UI {
     return book;
   }
 
-  removeBook(event) {
-    // remove element from interaface
-    const id = +event.target.parentElement.id;
-    event.target.parentElement.remove();
-    // remove element from array
-    const newBooks = this.books.filter((book) => book.id !== id);
-    this.books = newBooks;
-    localStorage.setItem('books', JSON.stringify(this.books));
-    return this.books;
-  }
-
   createRemoveBtn() {
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
-    removeBtn.addEventListener('click', this.removeBook);
+    removeBtn.addEventListener('click', (e) => {
+      // remove element from interaface
+      const id = +e.target.parentElement.id;
+      e.target.parentElement.remove();
+      // remove element from array
+      const newBooks = this.books.filter((book) => book.id !== id);
+      this.books = newBooks;
+      localStorage.setItem('books', JSON.stringify(this.books));
+      return this.books;
+    });
+
     return removeBtn;
   }
 
